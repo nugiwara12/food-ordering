265671b2cbefd22c76b46fea4730b3bb374aa35f
Nugiwara_011200 <global-config@example.com>
2024-06-05 00:33:17 +0800

    add menu for kapampangan

--- components/Navbar/NavbarOutside.js
+++ components/Navbar/NavbarOutside.js
@@ -8,7 +8,7 @@ const NavbarOutside = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 mb-4">
       <div className="container mx-auto flex items-center justify-between">
         <div className="flex items-center">
           <button
