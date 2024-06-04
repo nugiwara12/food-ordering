8ce769bb02aac4a71ee1bf9e338245d4ab7037dc
Nugiwara_011200 <global-config@example.com>
2024-06-05 01:18:30 +0800

    add kapampangan foods transition color with shadow

---
+++ src/app/Home/Menu/Kapampangan/page.js
@@ -0,0 +1,128 @@
+import React from "react";
+import NavbarOutside from "../../../../../components/Navbar/NavbarOutside";
+import { BiArrowBack } from "react-icons/bi";
+
+const Kapampangan = () => {
+  return (
+    <>
+      <NavbarOutside />
+      <div className="ml-4 hover:text-blue-700 transform">
+        <a href="/Home/Menu">
+          <BiArrowBack className="h-6 w-6" />
+        </a>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-4 aspect-auto">
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/adobo.png"
+            alt="Adobo"
+            className="h-40 w-40"
+          />
+          <h2 className="text-lg font-bold mt-2">Adobo</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/adobo.png"
+            alt="Sinigang"
+            className="h-40 w-40"
+          />
+          <h2 className="text-lg font-bold mt-2">Sinigang</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/sisig.png"
+            alt="Leche Flan"
+            className="h-36 w-40 rounded-full"
+          />
+          <h2 className="text-lg font-bold mt-2">Sisig</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/sisig.png"
+            alt="Leche Flan"
+            className="h-36 w-40 rounded-full"
+          />
+          <h2 className="text-lg font-bold mt-2">Sisig</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/sisig.png"
+            alt="Leche Flan"
+            className="h-36 w-40 rounded-full"
+          />
+          <h2 className="text-lg font-bold mt-2">Sisig</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/sisig.png"
+            alt="Leche Flan"
+            className="h-36 w-40 rounded-full"
+          />
+          <h2 className="text-lg font-bold mt-2">Sisig</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/sisig.png"
+            alt="Leche Flan"
+            className="h-36 w-40 rounded-full"
+          />
+          <h2 className="text-lg font-bold mt-2">Sisig</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/sisig.png"
+            alt="Leche Flan"
+            className="h-36 w-40 rounded-full"
+          />
+          <h2 className="text-lg font-bold mt-2">Sisig</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-gray-200 transition-colors duration-300 ease-in-out hover:bg-white shadow-lg hover:shadow-lg p-4">
+          <img
+            src="/menu/kapampangan/sisig.png"
+            alt="Leche Flan"
+            className="h-36 w-40 rounded-full"
+          />
+          <h2 className="text-lg font-bold mt-2">Sisig</h2>
+          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
+          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
+            Order Now
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Kapampangan;
--- src/app/Home/Menu/page.js
+++ src/app/Home/Menu/page.js
@@ -5,44 +5,21 @@ const Menu = () => {
   return (
     <>
       <NavbarOutside />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-4 aspect-auto">
-        <div className="flex flex-col justify-center items-center bg-gray-300 shadow-lg p-4">
-          <img
-            src="/menu/kapampangan/adobo.png"
-            alt="Adobo"
-            className="h-40 w-40"
-          />
-          <h2 className="text-lg font-bold mt-2">Adobo</h2>
-          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
-          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-            Order Now
-          </button>
-        </div>
-        <div className="flex flex-col justify-center items-center bg-gray-300 shadow-lg p-4">
-          <img
-            src="/menu/kapampangan/adobo.png"
-            alt="Sinigang"
-            className="h-40 w-40"
-          />
-          <h2 className="text-lg font-bold mt-2">Sinigang</h2>
-          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
-          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-            Order Now
-          </button>
-        </div>
-        <div className="flex flex-col justify-center items-center bg-gray-300 shadow-lg p-4">
-          <img
-            src="/menu/kapampangan/sisig.png"
-            alt="Leche Flan"
-            className="h-36 w-40 rounded-full"
-          />
-          <h2 className="text-lg font-bold mt-2">Sisig</h2>
-          <p className="text-sm text-gray-600 mb-4">A classic Filipino dish</p>
-          <button className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded">
-            Order Now
-          </button>
-        </div>
+      <div className="flex justify-center mb-4 gap-4">
+        <a
+          href="/Home/Menu/Kapampangan"
+          className="bg-gray-400 px-2 py-2 rounded-lg"
+        >
+          Kapampangan
+        </a>
+        <a href="#" className="bg-gray-400 px-2 py-2 rounded-lg">
+          Tagalog
+        </a>
+        <a href="#" className="bg-gray-400 px-2 py-2 rounded-lg">
+          Ilocano
+        </a>
       </div>
+      <div className="flex justify-center text-3xl font-bold">Menu Item</div>
     </>
   );
 };
