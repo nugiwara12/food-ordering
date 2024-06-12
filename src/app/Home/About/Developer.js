2a885e216dc8d0755b1df39f2c49bc731a7913a6
Nugiwara_011200 <global-config@example.com>
2024-06-12 22:02:22 +0800

    add developer page to call into global page

---
+++ src/app/Home/About/Developer.js
@@ -0,0 +1,202 @@
+import React from "react";
+import Image from "next/image";
+
+const Developer = () => {
+  return (
+    <>
+      <div className="py-20">
+        <div className="text-center mb-12 px-4">
+          <h2 className="text-3xl text-white md:text-4xl font-bold">
+            Our Developer
+          </h2>
+          <p className="text-gray-300 break-words mt-4 px-10">
+            Developers are not simply programmers; they are innovators who, with
+            every code word they write, reshape the digital world, propel
+            advancement, and improve our digital lives and jobs.
+          </p>
+        </div>
+
+        <div className="w-full flex flex-wrap justify-center mb-4 px-6">
+          <div className="max-w-sm w-full sm:w-1/2 md:w-1/3 bg-[#212121] border border-yellow-600 shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-center items-center py-4">
+              <Image
+                src="/developer/jcsarmiento.png"
+                alt="Jc Sarmiento"
+                width={200}
+                height={200}
+                className="w-40 h-40 rounded-full"
+              />
+            </div>
+            <div className="p-4">
+              <div className="font-semibold text-center text-sm text-white mb-2">
+                JC SARMIENTO
+              </div>
+              <div className="text-center text-sm font-normal mb-2 text-gray-400">
+                Web development is a dynamic and rewarding career that merges
+                technical expertise with creativity to create innovative
+                solutions that shape the digital landscape.
+              </div>
+              <div className="font-bold text-end text-sm text-gray-400">
+                Web <span className="text-yellow-600">Developer</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-center gap-4 px-6">
+          <div className="max-w-sm w-full sm:w-1/2 md:w-1/3 bg-[#212121] border border-yellow-600 shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-center items-center py-4">
+              <Image
+                src="/developer/edgar.png"
+                alt="Edgar Tabunda"
+                width={200}
+                height={200}
+                className="w-40 h-40 rounded-full"
+              />
+            </div>
+            <div className="p-4">
+              <div className="font-semibold text-center text-sm text-white mb-2">
+                EDGAR TABUNDA
+              </div>
+              <div className="text-center text-sm font-normal mb-2 text-gray-400">
+                Web development involves creating, maintaining, and optimizing
+                websites and applications, requiring expertise in programming
+                languages, design principles, and user experience, blending
+                creativity, technical expertise, and problem-solving.
+              </div>
+              <div className="font-bold text-end text-sm text-gray-400">
+                Web <span className="text-yellow-600">Developer</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm w-full sm:w-1/2 md:w-1/3 bg-[#212121] border border-yellow-600 shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-center items-center py-4">
+              <Image
+                src="/developer/reinier.png"
+                alt="Reinier Mercado"
+                width={200}
+                height={200}
+                className="w-40 h-40 rounded-full"
+              />
+            </div>
+            <div className="p-4">
+              <div className="font-semibold text-center text-sm text-white mb-2">
+                REINIER MERCADO
+              </div>
+              <div className="text-center text-sm font-normal mb-2 text-gray-400">
+                Mobile developers are a dynamic and rewarding career that drives
+                innovation and shapes the digital landscape, enhancing
+                connectivity and productivity in the ever-evolving technology
+                landscape.
+              </div>
+              <div className="font-bold text-end text-sm text-gray-400">
+                Mobile <span className="text-yellow-600">Developer</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm w-full sm:w-1/2 md:w-1/3 bg-[#212121] border border-yellow-600 shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-center items-center py-4">
+              <Image
+                src="/developer/dominic.png"
+                alt="Dominic Jubinal"
+                width={200}
+                height={200}
+                className="w-40 h-40 rounded-full"
+              />
+            </div>
+            <div className="p-4">
+              <div className="font-semibold text-center text-sm text-white mb-2">
+                DOMINI JUBINAL
+              </div>
+              <div className="text-center text-sm font-normal mb-2 text-gray-400">
+                Mobile development faces challenges like app performance,
+                security, and user data protection, but the rewards are
+                significant as an app transforms from concept to dependable
+                product.
+              </div>
+              <div className="font-bold text-end text-sm text-gray-400">
+                Mobile <span className="text-yellow-600">Developer</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm w-full sm:w-1/2 md:w-1/3 bg-[#212121] border border-yellow-600 shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-center items-center py-4">
+              <Image
+                src="/developer/julian.png"
+                alt="Julian Anunciacion"
+                width={200}
+                height={200}
+                className="w-40 h-40 rounded-full"
+              />
+            </div>
+            <div className="p-4">
+              <div className="font-semibold text-center text-sm text-white mb-2">
+                JULIAN ANUNCIACION
+              </div>
+              <div className="text-center text-sm font-normal mb-2 text-gray-400">
+                UI and UX design are crucial for digital product success,
+                ensuring visual appeal, functionality, and user experience, and
+                investing in good design is essential in the rapidly evolving
+                digital landscape.
+              </div>
+              <div className="font-bold text-end text-sm text-gray-400">
+                UI<span className="text-yellow-600">/UX</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm w-full sm:w-1/2 md:w-1/3 bg-[#212121] border border-yellow-600 shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-center items-center py-4">
+              <Image
+                src="/developer/kenji.png"
+                alt="Kenji Hidaka"
+                width={200}
+                height={200}
+                className="w-40 h-40 rounded-full"
+              />
+            </div>
+            <div className="p-4">
+              <div className="font-semibold text-center text-sm text-white mb-2">
+                KENJI HIDAKA
+              </div>
+              <div className="text-center text-sm font-normal mb-2 text-gray-400">
+                DevOps is a transformative approach that improves software
+                development speed, reliability, and security by fostering
+                collaboration and continuous improvement, benefiting users and
+                businesses.
+              </div>
+              <div className="font-bold text-end text-sm text-gray-400">
+                Dev<span className="text-yellow-600">Ops</span>
+              </div>
+            </div>
+          </div>
+          <div className="max-w-sm w-full sm:w-1/2 md:w-1/3 bg-[#212121] border border-yellow-600 shadow-lg rounded-lg overflow-hidden">
+            <div className="flex justify-center items-center py-4">
+              <Image
+                src="/developer/vincent.png"
+                alt="Vincent Jay Guiao"
+                width={200}
+                height={200}
+                className="w-40 h-40 rounded-full"
+              />
+            </div>
+            <div className="p-4">
+              <div className="font-semibold text-center text-sm text-white mb-2">
+                VINCENT JAY GUIAO
+              </div>
+              <div className="text-center text-sm font-normal mb-2 text-gray-400">
+                DevOps fosters collaboration, shared responsibility, open
+                communication, and continuous learning, leading to improved
+                morale, increased productivity, and alignment between business
+                objectives and IT capabilities.
+              </div>
+              <div className="font-bold text-end text-sm text-gray-400">
+                Dev<span className="text-yellow-600">Ops</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Developer;
