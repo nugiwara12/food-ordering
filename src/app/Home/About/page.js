3e29a6ca0b489cac8fafaf273e10f92d7ab65181
Nugiwara_011200 <global-config@example.com>
2024-06-12 22:02:46 +0800

    add global page to easy call the all pages of the about

--- src/app/Home/About/page.js
+++ src/app/Home/About/page.js
@@ -1,12 +1,21 @@
 import React from "react";
 import NavbarOutside from "../../../../components/Navbar/NavbarOutside";
 import Timeline from "./Timeline";
+import Developer from "./Developer";
+import Footer from "../../../../components/Footer/Footer";
 
 const About = () => {
   return (
     <>
       <NavbarOutside />
-      <Timeline />
+      <main
+        className="bg-cover bg-center bg-no-repeat min-h-screen"
+        style={{ backgroundImage: 'url("/logo/background.jpg")' }}
+      >
+        <Timeline />
+        <Developer />
+        <Footer />
+      </main>
     </>
   );
 };
